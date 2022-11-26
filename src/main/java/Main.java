21e0d9d55b2c5feb427fdc53c9717cdda081350a
Masha Sechina <mashka_3@mail.ru>
2022-11-26 19:53:49 +0300

    stats2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import ru.netology.stats.StatsService;
 public class Main {
 
     public static void main(String[] args) {
-        int[] sales ;
+        int[] sales;
 
         StatsService service = new StatsService();
     }
